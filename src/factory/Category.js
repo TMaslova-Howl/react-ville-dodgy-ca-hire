64b1a1ad84f5a63783c2efb115d985b5a2522b51
TMaslova-Howl <113369945+TMaslova-Howl@users.noreply.github.com>
2024-06-21 13:13:40 +0100

    remove unused items

--- src/factory/Category.js
+++
@@ -1,8 +0,0 @@
-const Category = {
-    Small: {name: "Small", price: 20},
-    SUV: {name: "SUV", price: 35},
-    Van: {name: "Van", price: 50},
-    Sport: {name:"Sport", price:200}
-};
-
-export default Category;
--- src/factory/Vehicle.js
+++
@@ -1,6 +0,0 @@
-const Vehicle = (make, model, category, hired) => {
-    return ({
-        make, model, category, hired
-    });
-};
-export default Vehicle;
